a2000428e7216e817771d7b62baf1f485173057c
your <robsonds28@gmail.com>
2021-04-01 16:38:46 -0300

    aula

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Column(
-        children: [
-          Container(
-            height: 200,
-            width: 200,
-            color: Colors.blue
-          ),
-          Container(),
-          Container(),
+        children:[
+          ColorfullContainer(),
+          ColorfullContainer(),
+          ColorfullContainer(),
         ],
       ),
+    );
+  }
+}
+
+class ColorfullContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 250,
+      color: Colors.white,
+      margin: EdgeInsets.all(20),
     );
   }
 }
